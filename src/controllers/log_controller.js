5e62e781c32127f10b3ae0f96dc0b7f2bce66e23
Arun Kumar Nair <arunnair018@gmail.com>
2022-12-06 21:45:27 +0530

    added no server configured

--- src/controllers/log_controller.js
+++ src/controllers/log_controller.js
@@ -8,6 +8,6 @@ export const create_log = async (req, res) => {
 };
 
 export const parse_branches_from_env = async (req, res) => {
-  const server_branches = process.env.SERVER_BRANCHES.split(',');
+  const server_branches = process?.env?.SERVER_BRANCHES?.split(',') || [];
   res.status(200).json(server_branches);
 };
